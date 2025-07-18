a85d52556409a8ad5481477fcbc692e426395947
Soumo275 <soumostudy27@gmail.com>
2025-07-18 19:10:55 +0530

    deployment update

--- app/app.py
+++ app/app.py
@@ -151,4 +151,4 @@ def serve_index():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
